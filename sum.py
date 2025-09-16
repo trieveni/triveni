e3a6482acaf2d1958a6a5fd61e4bd72ce5116586
Mahamkali Triveni <mahamkalitriveni92@gmail.com>
2025-09-16 11:03:03 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,52 @@
+"""num1 = int(input())
+num2 = int(input())
+print(f"sum:{num1+num2}")
+
+
+radius = int(input("give radius:"))
+a = 3.14*(radius**2)
+print("area of the circle:",a,sep="")
+print(f"area of the circle:{a}")
+
+a = int(input("give a:"))
+b = int(input("give b:"))
+c = int(input("give c:"))
+
+root1=0
+root2=0
+
+
+d = (b**2)-4*a*c
+root1=(-b+(d**(0.5)))/2*a
+root2=(-b-(d**(0.5)))/2*a
+
+print(f"roots:({root1},{root2})")
+a = int(input("give a:"))
+b = int(input("give b:"))
+temp = b
+b = a
+a = temp
+print(f"value of a is:{a}")
+print(f"value of b is:{b}")"""
+a = int(input("give a:"))
+b = int(input("give b:"))
+b = b+a
+a = b-a
+b = b-a
+
+print(f"value of a is:{a}")
+print(f"value of b is:{b}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
